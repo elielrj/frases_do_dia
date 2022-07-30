540245964a30b44b2deba24fcd1e51ac4b6dd6ab
Eliel <elielrj@gmail.com>
2022-07-30 08:14:09 -0300

    text

--- main.dart
+++ main.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(color: Colors.white),
+    home: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "texto",
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0,
+                  wordSpacing: 0,
+                  decoration: TextDecoration.lineThrough,
+                  decorationColor: Colors.amber,
+                ),
+              )
+            ],
+          ),
+        )),
   ));
 }
